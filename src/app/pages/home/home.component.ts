ec66e872985e29fbdabdcf9a0e16c39ede3801b8
Chaofan Zhou <chaofan_zhou@epam.com>
2024-02-29 15:03:35 +0800

    feat: add homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import sourceData from './data';
 
+interface Item {
+  id: number;
+  name: string;
+  photos: string[];
+  content: string;
+  tag: string;
+  nickname: string;
+  avatar: string;
+  starCount: number;
+  favorite: boolean;
+}
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AsyncPipe, MatIconModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  itemList$ = new BehaviorSubject<Item[]>([]);
 
+  ngOnInit(): void {
+    this.fetchItemList();
+  }
+
+  fetchItemList() {
+    this.itemList$.next(sourceData as Item[]);
+  }
 }
