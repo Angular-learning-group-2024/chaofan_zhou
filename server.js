159a727b748a3974787f2d30397e1e0b9d1f7996
Chaofan Zhou <chaofan_zhou@epam.com>
2024-03-21 16:50:35 +0800

    feat: mobile adaptation

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
-
+ 
 const app = express();
 app.use(cors());
-app.use(express.json()); // For JSON request bodies
-
+app.use(express.json());
+ 
 const pool = new Pool({
   user: 'azureuser',
   host: '10.74.67.197',
@@ -13,53 +13,49 @@ const pool = new Pool({
   password: '1qaz@WSX',
   port: 5434,
 });
-
+ 
 app.get('/data', async (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const limit = parseInt(req.query.limit) || 10;
   const offset = (page - 1) * limit;
   const keyword = req.query.keyword || '';
-
+ 
   let baseQuery = `
       SELECT t.*, array_agg(i.url) as img_urls
       FROM task t
-      LEFT JOIN imgs i ON i.task_id = t.id 
+      LEFT JOIN thumbnails i ON i.task_id = t.id
+      WHERE i.task_id IS NOT NULL
     `;
-
+ 
   if (keyword) {
-    baseQuery += `WHERE t.title LIKE '%${keyword}%' `;
+    baseQuery += `AND t.title LIKE '%${keyword}%' `;
   }
 
-  // 加入GROUP BY和ORDER BY子句
   baseQuery += ` GROUP BY t.id ORDER BY t.id `;
 
-  // 加入分页查询
   const paginatedQuery = `${baseQuery} LIMIT $1 OFFSET $2`;
 
   try {
-    // 发出查询请求
     const result = await pool.query(paginatedQuery, [limit, offset]);
-    const filterResult = result.rows.filter((item) => {
-      return item.img_urls[0] !== null
-    })
+    const filtedResult = result.rows.filter(item => item.img_urls.length > 0 )
     res.json({
       page,
       limit,
-      data: filterResult,
+      data: filtedResult,
     });
   } catch (error) {
     console.error('Error executing query', error.stack);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
+ 
 app.get('/data/:id', async (req, res) => {
   const { id } = req.params;
   try {
     const client = await pool.connect();
     try {
       await client.query('BEGIN');
-      const result = await client.query('SELECT t.*, array_agg(i.url) as img_urls FROM task t  LEFT JOIN imgs i ON i.task_id = t.id where t.id = $1 GROUP BY t.id', [id]);
+      const result = await client.query('SELECT t.*, array_agg(i.url) as img_urls FROM task t  LEFT JOIN thumbnails i ON i.task_id = t.id where t.id = $1 GROUP BY t.id', [id]);
       await client.query('COMMIT');
       res.json(result.rows.length > 0 ? result.rows[0] : "");
     } catch (e) {
@@ -74,6 +70,34 @@ app.get('/data/:id', async (req, res) => {
   }
 });
 
+app.post('/accept', async (req, res) => {
+  const { id, newState } = req.body;
+
+  try {
+      const queryText = 'UPDATE task SET status = $1 WHERE id = $2';
+      const result = await pool.query(queryText, [newState, id]);
+      console.log(result)
+      res.send('Data updated successfully');
+  } catch (error) {
+      console.error('Error:', error);
+      res.status(500).send('Error updating data');
+  }
+});
+
+app.post('/reject', async (req, res) => {
+  const { id, newState, comments } = req.body;
+
+  try {
+      const queryText = 'UPDATE task SET status = $1, comments = $2 WHERE id = $3';
+      const result = await pool.query(queryText, [newState, comments, id]);
+      console.log(result)
+      res.send('Data updated successfully');
+  } catch (error) {
+      console.error('Error:', error);
+      res.status(500).send('Error updating data');
+  }
+});
+ 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
